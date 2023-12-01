436544dece086a5119ed0d4569c2bf168c59ecad
vjvara27@colby.edu <vjvara27@colby.edu>
2023-12-01 23:48:21 +0000

    Edited Analyze_Topics_Form

--- client_code/Analyze_Topics_Form/__init__.py
+++ client_code/Analyze_Topics_Form/__init__.py
@@ -24,14 +24,14 @@ class Analyze_Topics_Form(Analyze_Topics_FormTemplate):
       
   def corpus_topic_model_button_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
-    self.corpus_plot_2.c = None
+    self.corpus_plot_2.figure = None
     wc = anvil.server.call('get_topic_model_topics_plot')
     self.plot_cloud(wc, 'Topic')
     self.corpus_topic_model_button.selected = False
 
   def corpus_topic_documents_model_button_clicked(self, **event_args):
     """This method is called when this radio button is selected"""
-    self.corpus_plot_2.figure.clear()
+    self.corpus_plot_2.figure = None 
     wc = anvil.server.call('get_topic_model_documents_plot')
     self.plot_cloud(wc, 'Document & Topics')
     self.corpus_topic_documents_model_button.selected = False
